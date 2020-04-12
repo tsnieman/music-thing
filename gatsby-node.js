11341082203b0d8c53aaedb4fbbdf8f25f0c438b
Tyler Nieman <tyler@tsnieman.net>
2020-04-12 10:09:29 -0700

    :card_file_box: use gatsby-node for static discogs data

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,38 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const fetch = require(`node-fetch`)
+
+const DISCOGS_TOKEN = `maSnEZtPwLRMXfgzfpjkAMeZMXvzendkTldZVKup`;
+
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest,
+}) => {
+  // get data from Discogs at build time
+  const result = await fetch(`https://api.discogs.com/users/tsnieman/collection/folders/0/releases?token=${DISCOGS_TOKEN}`)
+  const resultData = await result.json()
+  const releases = [...resultData.releases];
+  for (let i = resultData.pagination.page; i < resultData.pagination.pages; i++) {
+    const pageResult = await fetch(`${resultData.pagination.urls.next}&token=${DISCOGS_TOKEN}`);
+    const {
+      releases: releasePage,
+    } = await pageResult.json();
+    releases.push(...releasePage);
+  }
+
+  // create node for build time data
+  createNode({
+    // Queryable fields
+    releases,
+
+    // required fields (for createNode)
+    id: `discogs-build-time-data`,
+    parent: null,
+    children: [],
+    internal: {
+      type: `DiscogsReleases`,
+      contentDigest: createContentDigest(resultData),
+    },
+  })
+}
