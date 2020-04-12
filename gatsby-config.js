1044fa086fdd24eb295282bf8143650ba01beadc
Tyler Nieman <tyler@tsnieman.net>
2020-04-12 10:12:19 -0700

    :tada: this is yours? this is mine

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Music thing`,
+    description: `just playing around with a music thing`,
+    author: `@tsnieman`,
   },
   plugins: [
     `gatsby-plugin-theme-ui`,
