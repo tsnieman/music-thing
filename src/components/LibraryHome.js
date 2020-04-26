6db50b3f26285ed69801f396ba99bddb2b368e65
Tyler Nieman <tyler@tsnieman.net>
2020-04-25 18:46:29 -0700

    :bug: testing if i can detect SPA loading status

--- src/components/LibraryHome.js
+++ src/components/LibraryHome.js
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx, Box, Text } from 'theme-ui'
+import { Location } from '@reach/router'
 
 import SEO from '../components/seo'
 
@@ -9,6 +10,15 @@ const LibraryHomePage = () => {
       <SEO title="Library Home" />
 
       <Text>Library home</Text>
+
+      <Location>
+        {({ location }) => (
+          <div>
+            {location.pathname}
+            {console.log({ location })}
+          </div>
+        )}
+      </Location>
     </Box>
   )
 }
